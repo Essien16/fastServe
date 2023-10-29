9ff4f592b4285f1613df2bc664ca2047b15d5524
Justice Essien <essienjustice@gmail.com>
2023-10-29 07:50:21 +0100

    minor fix

--- src/models/Restaurant.ts
+++ src/models/Restaurant.ts
@@ -3,13 +3,13 @@ const { Schema, model } = mongoose
 
 const restaurantSchema = new Schema({
   user_id: { type: Schema.Types.ObjectId, ref: "users", required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   description: { type: String },
   cover: { type: String, required: true },
   address: { type: String, required: true },
   status: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
-})
+});
 
 export default model("restaurants", restaurantSchema)
